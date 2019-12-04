beefd98461d59527b9d91107d8a21bb45d8bb2a5
Adnan <adnanh@ridlr.in>
2019-12-04 19:16:00 +0530

    More functions to SingleLL

--- src/com/adnan/DataStructure/SingleLinkedList/SingleLLTest.java
+++ src/com/adnan/DataStructure/SingleLinkedList/SingleLLTest.java
@@ -4,11 +4,8 @@ public class SingleLLTest {
 	public static void main(String args[]) {
 		SingleList<Integer> sl = new SingleList<>();
 		System.out.println("IsEmpty: " + sl.isEmpty());
-		sl.append(1);
-		sl.append(2);
-		sl.append(3);
-		sl.append(4);
-		sl.append(5);
+		sl.append(1); sl.append(2); sl.append(3); 
+		sl.append(4); sl.append(5);
 		System.out.println("IsEmpty: " + sl.isEmpty() + " = " + sl.toString());
 
 		sl.prepend(-1);
@@ -25,9 +22,24 @@ public class SingleLLTest {
 		sl.removeLast();
 		sl.removeLast();
 		sl.append(11);
-		System.out.println("Remove Last then add = " + sl.toString());
-
+		System.out.println("Remove 2 Last then add 11 = " + sl.toString());
 		// sl.insert(10, 12);
+
+		sl.removeFirst();
+		System.out.println("Remove First = " + sl.toString());
+
+		SingleList<Integer>.Node<Integer> n101 = sl.new Node<>(101);
+		SingleList<Integer>.Node<Integer> n102 = sl.new Node<>(102);
+		sl.append(n101);
+		sl.append(n102);
+
+		System.out.println("Added with node pointer = " + sl.toString());
+		
+		sl.remove(n101);
+		System.out.println("Removed 101 with node pointer = " + sl.toString());
+		// sl.remove(n102);
+		
+		System.out.println("Find kth last 2: " + sl.findKthLast(2) + " | 10 : " + sl.findKthLast(10));
 	}
 }
 
@@ -36,7 +48,7 @@ class SingleList<T> {
 	private Node<T> head;
 	private int size = 0;
 
-	private class Node<T> {
+	class Node<T> {
 		T data;
 		Node<T> next;
 
@@ -58,6 +70,22 @@ class SingleList<T> {
 		}
 		Node<T> node = head;
 		while(node.next != null) {
+			node = node.next;
+		}
+
+		node.next = newNode;
+		size++;
+	}
+
+	public void append(Node<T> newNode) {
+		if (head == null) {
+			head = newNode;
+			size++;
+			return;
+		}
+
+		Node<T> node = head;
+		while (node.next != null) {
 			node = node.next;
 		}
 
@@ -140,6 +168,61 @@ class SingleList<T> {
 		size--;
 		return data;
 	}
+
+	public T removeFirst() {
+		if (head == null) {
+			return null;
+		}
+
+		Node<T> node = head;
+		head = head.next;
+		T data  = node.data;
+		node = null;
+		return data;
+	}
+
+	/* IMP */
+	public void remove(Node<T> node) {
+		if (node == null) {
+			return;
+		}
+
+		if (node.next == null) {
+			throw new IllegalStateException("Cannot delete last node with direct reference");
+		}
+
+        Node<T> nextNode = node.next;
+        node.data = nextNode.data;
+        node.next = nextNode.next;
+        nextNode = null;
+	}
+
+	public T findKthLast(int index) {
+		if (head == null) {
+			return null;
+		}
+
+		Node<T> node = head;
+		Node<T> kthNode = head;
+
+		int i = 1;
+		while(node.next != null) {
+			node = node.next;
+			if (i++ >= index) {
+				kthNode = kthNode.next;
+			}
+		}
+
+		if (i < index) {
+			return null;
+		}
+
+		return kthNode.data;
+	}
+
+	// findCycle (with 2 pointers slow and fast)
+	// kth last node (2 ways: 1. iterate first to get the size then iterate upto size - k 
+	// 2. Two pointers: second will start after first has moved kth position)
 
 	@Override
 	public String toString() {
